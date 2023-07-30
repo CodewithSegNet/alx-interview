cff28d013e375f033fc3a9000809cf9472c880fa
segunUb18 <eeomo@yahoo.com>
2023-07-31 00:32:31 +0100

    :white_check_mark: Utf8 validation
    task 0

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" a method that determines given data set repersents a valid UTF-8 encoding """
+
+
+def validUTF8(data):
+    """ checks utf8 valid data set """
+    for byt in data:
+        bin_char = format(byt, '08b')
+
+        if bin_char.startswith('0'):
+            continue
+        elif bin_char.startswith('110'):
+            continue
+        elif bin_char.startswith('1110'):
+            continue
+        elif bin_char.startswith('11110'):
+            continue
+        else:
+            return False
+    return True
